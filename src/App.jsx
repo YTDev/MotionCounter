c579ae32b5b7506370ae0be0e0a59ca8161b2dd2
YTDev <belnisay@gmail.com>
2025-02-17 02:07:47 +0000

    add from, to

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-3xl">
-        <Counter value={90000} />
+        {/* <Counter value={90000} /> */}
+        <Counter from={0} to={90000} />
       </div>
       <div className="h-screen flex items-center justify-center bg-blue-950 text-white text-3xl">
-        <Counter value={90000} />
+        <Counter from={90000} to={0} />
+        {/* <Counter value={90000} /> */}
       </div>
     </>
   );
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,10 +6,15 @@ import {
 } from "motion/react";
 import React, { useRef, useEffect } from "react";
 
-function Counter({ value }) {
+function Counter({ from = 0, to }) {
   const ref = useRef(null);
-  const motionCount = useMotionValue(0);
-  const springCount = useSpring(motionCount);
+  const motionCount = useMotionValue(from);
+
+  //control the oscillation to avoid dippin below 0
+  const springCount = useSpring(motionCount, {
+    damping: 80,
+    stiffness: 160,
+  });
   const isInView = useInView(ref, { once: true });
   //no need to use useEffect + springCount.on...+ cleanup function
   useMotionValueEvent(springCount, "change", (latest) => {
@@ -25,11 +30,25 @@ function Counter({ value }) {
   useEffect(() => {
     if (isInView) {
       //this line should start the spring animation
-      motionCount.set(value);
+      motionCount.set(to);
     }
-  }, [value, motionCount, isInView]);
+  }, [to, motionCount, isInView]);
 
   return <span ref={ref}></span>;
 }
 
 export default Counter;
+
+/*
+notes:
+
+Damping:
+Represents friction or resistance in the spring system. Higher damping means the motion settles faster with fewer 
+oscillations (less bounce), while lower damping results in more oscillation before coming to rest.
+
+Stiffness:
+Represents how strongly the spring forces the value back to its target. Higher stiffness makes the response 
+faster and more snappy, but if damping is low, it can cause overshoot; lower stiffness yields a gentler, slower movement.
+
+
+*/
