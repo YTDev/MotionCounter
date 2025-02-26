ca6940af64fe61c384c6b2ea03cda10ff0ea0da6
YTDev <belnisay@gmail.com>
2025-02-26 10:58:21 +0000

    fix: resolve 'the failed to resolve entry error'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: "src/index.js",
+
+      name: "MotionCounter",
+
+      fileName: (format) => `motioncounter.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "motion"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          motion: "MotionReact",
+        },
+      },
+    },
+  },
 });
